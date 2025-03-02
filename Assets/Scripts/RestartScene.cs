0466b90a0b10975088343f1e105dff9ef564a79a
Kaslanas <victor24274@gmail.com>
2025-03-02 09:29:06 -0500

    Script update

---
+++ Assets/Scripts/RestartScene.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartScene : MonoBehaviour
+{
+
+ public void Restart(){
+
+
+SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+
+
+}
+
+
+
+}
